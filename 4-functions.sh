7fcf64414d15b4552515daa22d15a6712c481734
Kamalesh-Kakarla <48642647+Kamalesh-Kakarla@users.noreply.github.com>
2023-08-03 09:14:50 +0530

    Update 4-functions.sh

--- 4-functions.sh
+++ 4-functions.sh
@@ -7,3 +7,13 @@ echo Good to have you here...
 
 greetings
 echo Function Exit Status - $?
+
+
+input(){
+echo First Argument - $1
+echo second Argument - $2
+echo All Arguments - $*
+echo No. of Arguments - $#
+}
+
+input KK 12
