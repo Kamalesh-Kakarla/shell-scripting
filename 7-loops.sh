31faca8cbe8f2e7549022eaf74ec5b9fc6c335ee
Kamalesh <kamaleshkakarla@gmail.com>
2023-08-03 10:05:39 +0530

    Loops

--- 7-loops.sh
+++ 7-loops.sh
@@ -9,3 +9,10 @@ while [ $a -gt 0 ]; do
   a=$(($a-1))
   #break # This command will break the flow
 done
+
+
+#For loop will run based on the inputs
+
+for comp in frontend catalogue user ; do
+  echo Install component - $comp
+done
